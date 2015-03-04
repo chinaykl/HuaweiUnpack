654c210bce40e1a367066250a59bdff723711072
zero <chinaykl@126.com>
2015-03-04 11:52:48 +0800

    Update repackbootimg.sh to make it simple

--- tool/linux/repackbootimg.sh
+++ tool/linux/repackbootimg.sh
@@ -1,5 +1,54 @@
 #!/bin/bash
 
+function help
+{
+	echo "help:"
+	echo "	repackbootimg.sh <'boot'||'recovery'>"
+}
+
+function pack
+{
+	echo "start to pack $target.img"
+	mkbootimg\
+	 --kernel $KERNEL\
+	 --ramdisk $RAMDISK\
+	 --cmdline "$CMDLINE"\
+	 --base $BASE\
+	 --pagesize $PAGESIZE\
+	 --dt $DT\
+	 --ramdisk_offset $RAMDISKOFF\
+	 --tags_offset $TAGSOFF\
+	 -o $OUTPATH
+	if [ $? -ne 0 ]
+	then
+		echo "pack $target.img fail"
+	else
+		echo "pack $target.img success"
+	fi
+}
+
+function check
+{
+	if [ $1 -ne 1 ]
+	then
+		echo "Wrong Argument!"
+		help
+		exit 1
+	fi
+
+	if [[ !($2 = "boot" || $2 = "recovery") ]]
+	then
+		echo "Wrong Argument $2"
+		help
+		exit 1
+	fi
+	
+	input=$2
+}
+
+check $# $1
+target=$input
+
 cd out
 
 if [ -e "boot.img" ]
@@ -7,14 +56,16 @@ then
 	rm boot.img
 fi
 
+INPATH="$target/$target.img"
+OUTPATH="$target.img"
+KERNEL="$INPATH-zImage"
+RAMDISK="$INPATH-ramdisk.gz"
 # androidboot.selinux=permissive
-mkbootimg\
- --kernel boot/boot.img-zImage\
- --ramdisk boot/boot.img-ramdisk.gz\
- --cmdline "androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci"\
- --base 0x80000000\
- --pagesize 2048\
- --dt boot/boot.img-dt\
- --ramdisk_offset 0x01000000\
- --tags_offset 0x00000100\
- -o boot.img		
+CMDLINE="androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci"
+BASE="0x80000000"
+PAGESIZE="2048"
+DT="$INPATH-dt"
+RAMDISKOFF="0x01000000"
+TAGSOFF="0x00000100"
+
+pack
